60bb86d66f3bad186d367bcdbdf7921ce445422a
kewoo1996 <47409020+kewoo1996@users.noreply.github.com>
2020-05-14 01:11:36 +0900

    Update ReservationView.java

--- src/main/java/projectStart/ReservationView.java
+++ src/main/java/projectStart/ReservationView.java
@@ -10,22 +10,32 @@ public class ReservationView {
         @Id
         @GeneratedValue(strategy=GenerationType.AUTO)
         private Long id;
+        private Long customerId;
         private String seatCount;
 
 
         public Long getId() {
             return id;
         }
-
+        
         public void setId(Long id) {
             this.id = id;
         }
-        public String getSeatCount() {
-            return seatCount;
+
+        public Long getCustomerId() {
+            return customerId;
         }
+    
+        public void setCustomerId(Long customerId) {
+            this.customerId = customerId;
+        }
+    
+        public String getSeatCount() {
+                return seatCount;
+            }
 
         public void setSeatCount(String seatCount) {
             this.seatCount = seatCount;
         }
-
+        
 }
