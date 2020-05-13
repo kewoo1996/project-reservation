ba24d738b1b07db15877590e329c16fb40501a99
kewoo1996 <47409020+kewoo1996@users.noreply.github.com>
2020-05-14 02:41:25 +0900

    Update ReservationViewRepository.java

--- src/main/java/projectStart/ReservationViewRepository.java
+++ src/main/java/projectStart/ReservationViewRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ReservationViewRepository extends CrudRepository<ReservationView, Long> {
-
+  
+  void deleteByCustomerId(Long customerId);
 
 }
